90a3963af6bb0e72dd022f1a4eb0ba592f0abbd8
Vconixx <novanta9061@gmail.com>
2026-07-04 21:26:57 +0300

    İlk ZencoLive projesi

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        backgroundColor: "#1e1f22",
+        color: "white",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+        fontWeight: "bold",
+      }}
+    >
+      ZencoLive 🚀
+    </main>
+  );
+}
